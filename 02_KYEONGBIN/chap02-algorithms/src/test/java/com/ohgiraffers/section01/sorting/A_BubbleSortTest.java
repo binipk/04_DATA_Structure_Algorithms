83c4dc33ecf9e9c52b407da362f7925dfab0a26c
binipk <mingyoku93@gmail.com>
2025-04-15 15:51:05 +0900

    update

---
+++ 02_KYEONGBIN/chap02-algorithms/src/test/java/com/ohgiraffers/section01/sorting/A_BubbleSortTest.java
@@ -0,0 +1,21 @@
+package com.ohgiraffers.section01.sorting;
+
+import org.junit.jupiter.api.BeforeAll;
+
+class A_BubbleSortTest {
+
+    @BeforeAll
+    public static void set() {
+
+
+        input1 = 7;
+        act1 = new int[]{34, 23, 5, 24, 1, 9, 12};
+        exp1 = new int[]{1, 5, 9, 12, 23, 24, 34};
+
+
+        input2 = 6;
+        act2 = new int[]{40, 47, 38, 8, 33, 35};
+        exp2 = new int[]{8, 33, 35, 38, 40, 47};
+    }
+
+}
